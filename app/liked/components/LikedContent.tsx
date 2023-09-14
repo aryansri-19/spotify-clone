7df1236c42b13a8a9dac255d61327385e9344c1a
Aryan Srivastava <aryansri.19@gmail.com>
2023-09-14 22:16:38 +0530

    temporary commit

---
+++ app/liked/components/LikedContent.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import LikeButton from "@/app/components/LikeButton";
+import MediaItem from "@/app/components/MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+interface LikedContentProps{
+    songs : Song[];
+}
+
+const LikedContent: React.FC<LikedContentProps> = ({
+    songs
+}) => {
+    const onPlay = useOnPlay(songs);
+    const router = useRouter();
+    const { isLoading, user } = useUser();
+
+    useEffect(()=>{
+        if(!isLoading && !user){
+            router.replace('/');
+        }
+    }, [isLoading, user, router]);
+
+    if(songs.length === 0){
+        return (
+            <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+                No Liked Songs
+            </div>
+        )
+    }
+
+    return ( 
+        <div className="flex flex-col gap-y-2 w-full p-6">
+            {songs.map((item)=>(
+                <div key={item.id} className="flex items-center gap-x-4 w-full">
+                    <div className="flex-1">
+                        <MediaItem onClick={(id:string)=>onPlay(id)} data={item}/>
+                    </div>
+                    <LikeButton songId={item.id}/>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default LikedContent;
